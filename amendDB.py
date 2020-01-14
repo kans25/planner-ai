f2dee7b67c16453bfa538764bb8faf316341ade9
Kanaee <30886525+kans25@users.noreply.github.com>
2020-01-15 03:08:55 +0800

    Add files via upload

--- amendDB.py
+++ amendDB.py
@@ -1,5 +1,6 @@
 import sqlite3
 import scheduling
+import datetime
 
 def add_user( username, email ): 
 	# connecting to the database  
@@ -13,13 +14,11 @@ def add_user( username, email ):
 	connection.close()
 
 def add_flexible( appointmentid, startdate, enddate, noofhours ):
-	# connecting to the database  
 	connection = sqlite3.connect("Planner.db") 
-	# cursor  
 	crsr = connection.cursor() 
 	command = "INSERT INTO Flexible (AppointmentID, StartDate, EndDate, NoOfHours) VALUES( "
-	command += str(appointmentid) + ", " + str(startdate) + ", "
-	command += ", " + str(enddate) + ", " + str(noofhours) + "\" );"
+	command += str(appointmentid) + ", " + str(startdate)
+	command += ", " + str(enddate) + ", " + str(noofhours) + " );"
 	crsr.execute(command)  
 	connection.commit() 
 	# close the connection 
@@ -84,21 +83,30 @@ def delete_timeslot( appointmentid ):
 	connection.close()
 
 
-def add_appointment( userid, title, isflexible, iscomplete, notes, alert, invitees, location ): 
+def add_appointment( userid, title, isflexible, iscomplete, notes, alert, invitees, location, start, end, hours, date ): 
 	connection = sqlite3.connect("Planner.db") 
 	crsr = connection.cursor() 
 	command = "INSERT INTO Appointment (UserID, Title, isFlexible, isComplete, Notes, Alert, Invitees, Location) VALUES ( "
 	command += str(userid) + ", \"" + str(title) + "\", " + str(isflexible) 
 	command += ", " + str(iscomplete) + ", \"" + str(notes) + "\", " + str(alert) + ", " 
 	command += str(invitees) + ", \"" + str(location) + "\");"
-	crsr.execute(command) 
+	#crsr.execute(command) 
 	connection.commit() 
 	connection.close()
 
-	# if isflexible == 1: #it is flexible
-	# 	add_flexible( sp.flexibleid, appointmentid, sp.startdate, sp.enddate, sp.hours)
-	# else: #it is not flexible
-	# 	add_notflexible(sp.notflexibleid, appointmentid, sp.date, sp.starttime, sp.endtime)
+	connection2 = sqlite3.connect("Planner.db") 
+	crsr2 = connection2.cursor() 
+	command = "SELECT MAX(AppointmentID) FROM Appointment;"
+	AppointmentID = crsr2.execute(command)
+	rows = crsr2.fetchall()
+	connection2.commit() 
+	connection2.close()
+	row = rows[0]
+
+	if isflexible == 1: #it is flexible
+		add_flexible( rows[0][0], start, end, hours )
+	else: #it is not flexible
+		add_notflexible( rows[0][0], date, start, end )
 	# 	add_timeslot(sp.timeslotid, appointmentid, sp.starttime, sp.endtime)
 
 
@@ -118,9 +126,11 @@ def delete_appointment( appointmentid ):
 
 
 
-add_user( "Kanaee", "kans@gmail.com")
-add_user( "Rhea", "rhea@gmail.com")
+#add_user( "Kanaee", "kans@gmail.com")
+#add_user( "Rhea", "rhea@gmail.com")
 #delete_user( 25 )
+
+#add_appointment( 2, "Adding Appointment", 1, 0, "Adding notes", 0, 1, "HKU", datetime.date(2020, 1, 15), datetime.date(2020, 1, 20), 10, 0)
 
 
 
